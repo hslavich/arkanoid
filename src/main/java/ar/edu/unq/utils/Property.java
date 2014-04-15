dad13a45ed725162a612d8090f5fb22e6bf8ac5d
hslavich <hernan.slavich@gmail.com>
2014-04-15 13:35:58 -0300

    Change game properties

--- src/main/java/ar/edu/unq/utils/Property.java
+++ src/main/java/ar/edu/unq/utils/Property.java
@@ -3,12 +3,12 @@ package ar.edu.unq.utils;
 import java.awt.Color;
 
 public class Property {
-	public static final int BALL_RADIUS = 10;
+	public static final int BALL_RADIUS = 8;
 	public static final Color BALL_COLOR = Color.RED;
 	public static final double BALL_SPEED = 300;
 
-	public static final int BAR_WIDTH = 80;
-	public static final int BAR_HEIGHT = 20;
+	public static final int BAR_WIDTH = 65;
+	public static final int BAR_HEIGHT = 18;
 	public static final Color BAR_COLOR = Color.BLUE;
-	public static final double BAR_SPEED = 200;
+	public static final double BAR_SPEED = 300;
 }
